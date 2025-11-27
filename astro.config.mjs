4a9c2f7219eb97001f52acc78cc7b2d5f692c39e
J-724R <jjpernian@gmail.com>
2025-11-27 09:53:33 -0400

    add framworks

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import preact from '@astrojs/preact';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [preact()]
+  integrations: [preact()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
